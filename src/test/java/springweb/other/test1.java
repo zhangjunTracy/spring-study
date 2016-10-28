f7bde27d28cb4be246c6b1f5e016e26b0c23e547
zhangjunTracy <13552960783@163.com>
2016-10-28 16:52:01 +0800

    Integer 缓存

---
+++ src/test/java/springweb/other/test1.java
@@ -0,0 +1,29 @@
+package springweb.other;
+
+public class test1 {
+    public static void main(String[] args) {
+
+        Integer a = 11;
+        Integer b = 11;
+
+        Integer c = 300;
+        Integer d = 300;
+
+        Integer integer;// -XX:AutoBoxCacheMax=<size>} 调解
+        Byte aByte;//
+
+        Long long1;//
+
+        Short short1;// -128-127
+
+        Character character;// 0-127
+
+        Float float1;
+
+        Double double1;
+
+        Boolean boolean1;
+
+    }
+
+}
