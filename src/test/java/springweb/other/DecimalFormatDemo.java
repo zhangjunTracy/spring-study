00ef635b0ada6ca13f0d403670f48f62bdc9a676
zhangjunTracy <13552960783@163.com>
2016-12-20 14:23:09 +0800

    格式化数字 用法

---
+++ src/test/java/springweb/other/DecimalFormatDemo.java
@@ -0,0 +1,43 @@
+package springweb.other;
+
+import java.text.DecimalFormat;
+
+import org.junit.Test;
+
+/**
+ * DecimalFormat 的用法 由于格式化double float浮点数类
+ * 
+ * @author Json
+ *
+ */
+// https://docs.oracle.com/javase/7/docs/api/java/text/DecimalFormat.html
+
+public class DecimalFormatDemo {
+
+    @Test
+    public void test() {
+        float a = 1f;
+        float b = a / 3;
+        System.out.println(b);
+        // 取整数位 没有返回0
+        System.out.println(new DecimalFormat("0").format(b));
+
+        // 0.00 表示四舍五入保留两位小数 如果不够用0补
+        // 取整数位和两位小数
+        System.out.println(new DecimalFormat("0.00").format(b));
+
+        // 取两位整数和三位小数，整数不足部分以0填补。
+        System.out.println(new DecimalFormat("00.000").format(b));
+
+        // 取所有整数部分 没有返回0
+        System.out.println(new DecimalFormat("#").format(b));
+
+        // 以百分比方式计数，并取两位小数
+        System.out.println(new DecimalFormat("0.00%").format(b));
+        System.out.println(new DecimalFormat("#.##%").format(b));
+        long c = 299792458;
+        // 每三位以逗号进行分隔。
+        System.out.println(new DecimalFormat(",###").format(c));//
+    }
+
+}
