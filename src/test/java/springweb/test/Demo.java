965d8ac6b6b9d23dd813c2d9088257e8d8974683
Json <Json@Json-PC>
2017-03-06 17:46:48 +0800

    KMP

---
+++ src/test/java/springweb/test/Demo.java
@@ -0,0 +1,11 @@
+package springweb.test;
+
+public class Demo {
+    public static void main(String[] args) {
+        String string = "abcdefg";
+        String string2 = "cde";
+        System.out.println(string.indexOf(string2));
+
+    }
+
+}
