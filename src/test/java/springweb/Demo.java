01bf8c82b2017d574bf695995efe289d0dbe3b4f
zhangjunTracy <13552960783@163.com>
2017-11-15 23:05:11 +0800

    demo

---
+++ src/test/java/springweb/Demo.java
@@ -0,0 +1,43 @@
+package springweb;
+
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Demo {
+
+    public static void main(String[] args) throws JsonProcessingException {
+
+        StringHttpMessageConverter m = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+        RestTemplate restTemplate = new RestTemplateBuilder().additionalMessageConverters(m).build();
+        HttpHeaders headers = new HttpHeaders();
+        MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
+        headers.setContentType(type);
+        headers.add("Accept", MediaType.APPLICATION_JSON.toString());
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("key", "2ef80a528f8f4040ba34c91ef314ef9a");
+        map.put("userid", "ddd");
+        map.put("info", ",,,");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonStr = mapper.writeValueAsString(map);
+        HttpEntity<String> formEntity = new HttpEntity<String>(jsonStr, headers);
+        ResponseEntity<String> entity = restTemplate.postForEntity("http://www.tuling123.com/openapi/api", formEntity,
+                String.class);
+
+        System.out.println(entity.getBody());
+    }
+
+}
