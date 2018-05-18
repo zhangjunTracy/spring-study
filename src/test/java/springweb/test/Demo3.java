98563f0c5da89791de294192af67434c55bdfd6d
zhangjunTracy <13552960783@163.com>
2018-05-18 13:20:03 +0800

    list TreeSet compartor and comparable

---
+++ src/test/java/springweb/test/Demo3.java
@@ -0,0 +1,32 @@
+package springweb.test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Demo3 {
+
+    public static void main(String[] args) {
+
+        Person person = new Person(1, "11");
+
+        Person person2 = new Person(1, "22");
+
+        System.out.println(person.equals(person2));
+        System.out.println(person.compareTo(person2) == 0);
+
+        SortedSet<Person> set = new TreeSet<>();
+        set.add(person);
+        set.add(person2);
+        System.out.println(set.size());
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("dd", "ddd");
+        map.put("dd", "dd");
+        map.put("dd", "dd");
+        System.out.println(map.toString());
+
+    }
+
+}
