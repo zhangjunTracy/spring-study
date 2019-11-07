0f08294ead216fa4967ac2d4b3285830fdef0035
zhangjun <zhangjun486@jd.com>
2019-11-07 10:52:40 +0800

    'd'

--- src/main/java/www/springweb/hello/aspect/AuthorizationAspect.java
+++ src/main/java/www/springweb/hello/aspect/AuthorizationAspect.java
@@ -1,12 +1,25 @@
 package www.springweb.hello.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
+import www.springweb.hello.annotation.Auth;
+
+import java.lang.reflect.Method;
 
 /**
  * @Aspect:指定是一个切面
@@ -29,7 +42,7 @@ public class AuthorizationAspect {
 
     /**
      * 需要认证的方法 在有@Auth注解的方法才进行验证 才走个方法 也可以使用execution表达式
-     * 
+     *
      * @param joinPoint
      */
     @Before("@annotation(www.springweb.hello.annotation.Auth)")
@@ -49,6 +62,47 @@ public class AuthorizationAspect {
         }
 
         System.out.println("Hello world for aspect111111111111");
+
+    }
+
+    @Around("AuthorizationAspect.anyPublicOperation1()")
+    private void apiAuth12(ProceedingJoinPoint joinPoint) throws NoSuchMethodException {
+
+        // 获取连接点的参数
+        Object[] strings = joinPoint.getArgs();
+        for (Object object : strings) {
+            System.out.println("==========:" + object);
+        }
+
+        System.out.println("Hello world for aspect111111111111");
+
+
+        Signature signature = joinPoint.getSignature();
+        if(!(signature instanceof MethodSignature)){
+            //出错
+        }
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        //拿到的时接口不是实现类
+        //methodSignature.getMethod();
+
+        Method method = joinPoint.getTarget().getClass().getMethod(methodSignature.getName(),
+                methodSignature.getMethod().getParameterTypes());
+
+        Auth annotation = method.getAnnotation(Auth.class);
+
+
+
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression expression = parser.parseExpression(annotation.value());
+        EvaluationContext context = new StandardEvaluationContext();
+        Object[] args=joinPoint.getArgs();
+        String[] params=new DefaultParameterNameDiscoverer().getParameterNames(method);
+        for (int i=0;i<args.length;i++){
+            context.setVariable(params[i],i);
+        }
+
+        String key=expression.getValue(context).toString();
+
 
     }
 }
